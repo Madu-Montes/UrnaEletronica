cc051b396d02673612c47b3d6e1e41e437191c4c
Madu-Montes <monteseduarda56@gmail.com>
2023-11-14 12:30:52 -0300

    iniciando o js

---
+++ js/etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: "JOSE DA SILVA",
+                partido: "PTB",
+                fotos: [
+                    { URL: '1.png', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: 38111,
+                nome: "JOSE DA SILVA",
+                partido: "PTB",
+                fotos: [
+                    { URL: '2.png', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: "JOSE DA SILVA",
+                partido: "PTB",
+                fotos: [
+                    { URL: '3.png', legenda: 'Prefeito' },
+                    { URL: '3.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: 38111,
+                nome: "JOSE DA SILVA",
+                partido: "PTB",
+                fotos: [
+                    { URL: '4.png', legenda: 'Prefeito' },
+                    { URL: '4.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+]
